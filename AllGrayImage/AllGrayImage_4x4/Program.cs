4ceece7e4e54d7c7655fc9a03e22d95d3f1f1910
NnWinter <515621078@163.com>
2023-08-24 07:28:39 +0800

    All Gray Image
    一个无意义的项目（大概）

---
+++ AllGrayImage/AllGrayImage_4x4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing.Imaging;
+
+var size = new Size(4, 4);
+var i = 0;
+
+var total = 1 << size.Width * size.Height;
+
+var outdir = "imgs";
+if (!Directory.Exists(outdir)) { Directory.CreateDirectory(outdir); }
+
+for (; i < total; i++)
+{
+    float[,] imageData = new float[size.Width, size.Height];
+    GenerateBinaryImageData(i, ref imageData);
+    Bitmap grayscaleImage = GenerateGrayscaleImage(imageData, size.Width, size.Height);
+    grayscaleImage.Save(Path.Combine(outdir,$"{i}.jpg"));
+}
+
+Bitmap GenerateGrayscaleImage(float[,] data, int width, int height)
+{
+    Bitmap image = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+    ColorPalette palette = image.Palette;
+    for (int i = 0; i < 256; i++)
+    {
+        palette.Entries[i] = Color.FromArgb(i, i, i);
+    }
+    image.Palette = palette;
+
+    BitmapData imageData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+
+    unsafe
+    {
+        byte* imagePointer = (byte*)imageData.Scan0;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int grayValue = (int)(data[y, x] * 255);
+                imagePointer[x] = (byte)grayValue;
+            }
+
+            imagePointer += imageData.Stride;
+        }
+    }
+
+    image.UnlockBits(imageData);
+
+    return image;
+}
+
+void GenerateBinaryImageData(int value, ref float[,] imageData)
+{
+    int width = imageData.GetLength(1);
+    int height = imageData.GetLength(0);
+
+    int index = 0;
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int binaryValue = (value >> index) & 1; // Get the current bit
+            float grayscaleValue = binaryValue * 1.0f; // Map 0 to 0.0 and 1 to 1.0
+            imageData[y, x] = grayscaleValue;
+            index++;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-<div style="color:#DDD; background-color:#1E1E1E; width:100%; height:100%">
-
 # NnResourceShare
+
 分享资源
 
-# 文件
-1. <a href=".\ISO-3166 + GBT-2659\">'ISO-3166' + 'GBT-2659-2000'</a><br/>
-    'The International Standard for country codes and codes for their subdivisions' + '世界各国和地区名称代码'<br/>
-    <div style="color:#FAA">
-    With this data, you can easily find the Chinese names corresponding to most countries in ISO-3166.<br/>
+## 文件
+
+1. ['ISO-3166' + 'GBT-2659-2000']('./ISO-3166+GBT-2659/')  
+    'The International Standard for country codes and codes for their subdivisions' + '世界各国和地区名称代码'  
+    With this data, you can easily find the Chinese names corresponding to most countries in ISO-3166.  
     通过这个数据，你可以找到 ISO-3166 中大多数国家所对应的中文名称。
-    </div>
-    Please note, some of the full English country names in this data are not accurate. It's more suitable for retrieving the corresponding Chinese short names and English short names rather than the full names.<br/>
-    这个数据中的国家英文全称中部分数据并不准确，更适合于检索对应的中文简称和国家英文简称而不是全称。
-</div>
+    Please note, some of the full English country names in this data are not accurate. It's more suitable for retrieving the corresponding Chinese short names and English short names rather than the full names.  
+    这个数据中的国家英文全称中部分数据并不准确，更适合于检索对应的中文简称和国家英文简称而不是全称。  
+    ![Sample](./ISO-3166+GBT-2659/sample.png)
+
+2. [All Gray Image / 灰色图]('./AllGrayImage/')  
+    Covers all possible grayscale pixel arrangement combinations in extremely small-sized images, including data redundancy, such as equivalent rotations and scalings.  
+    涵盖了极小尺寸图像中所有可能的灰度像素排列组合，包括冗余数据，如等价的旋转和缩放。  
+    ( currently 4x4 imgs only / 目前只有 4x4 尺寸 )
+    | 4x4_1081 | 4x4_10002 |
+    |:-----:|:-----:|
+    | ![sample_4x4_1081](AllGrayImage/AllGrayImage_4x4/sample/1081.jpg) | ![sample_4x4_10002](AllGrayImage/AllGrayImage_4x4/sample/10002.jpg) |
